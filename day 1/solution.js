465f6c97228733a5a0b8f8d024f9bf92e7915957
M4p4 <homejr@protonmail.com>
2022-12-05 05:06:30 +0100

    solution day 1

--- day 1/solution.js
+++ day 1/solution.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const path = require('path');
+const fs = require('fs');
+// load input
+const elves = fs
+  .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+  .trim()
+  .split('\n\n');
+
+const sorted_elves = elves
+  .map((elf) => {
+    return elf
+      .split('\n')
+      .map((item) => Number.parseInt(item))
+      .reduce((sum, val) => sum + val, 0);
+  })
+  .sort((a, b) => b - a);
+
+console.log(sorted_elves[0]);
